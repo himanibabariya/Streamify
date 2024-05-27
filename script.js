c4e88d04b43e965c248f9eb2123e550b45a862fe
Himani Babariya <himanibabariya@gmail.com>
2024-05-27 17:03:51 -0400

    Songs added

---
+++ script.js
@@ -0,0 +1,50 @@
+async function getSongs(){
+    let a = await fetch("http://192.168.180.2:3000/songs")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML=response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/songs/")[1])
+        }
+    }
+    return songs
+}
+const playMusic = (track) =>{
+    let audio = new Audio("/songs/" + track)
+audio.play();
+}
+async function main(){
+    let currentSong;
+    //gett the list of all the songs
+    let songs = await getSongs()
+
+    //show all the songs in the playlist
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+      songUL.innerHTML = songUL.innerHTML + `<li> 
+      
+                            <img src="images/music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>Himani</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="images/play.svg" alt="">
+                            </div></li>`;  
+    }
+//attch an event listner to an each song
+Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+    e.addEventListener("click", element=>{
+        console.log(e.querySelector(".info").firstElementChild.innerHTML)
+        playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    })
+})
+
+
+}
+main()
