f0d268aeb7816801d7b8a5c48d8e3b79e3398ca2
Himani Babariya <himanibabariya@gmail.com>
2024-05-28 23:02:46 -0400

    Song info and Duration added

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-async function getSongs(){
+
+let currentSong = new Audio();
+async function getSongs() {
     let a = await fetch("http://192.168.180.2:3000/songs")
     let response = await a.text();
     let div = document.createElement("div")
-    div.innerHTML=response;
+    div.innerHTML = response;
     let as = div.getElementsByTagName("a")
     let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs
 }
-const playMusic = (track) =>{
-    let audio = new Audio("/songs/" + track)
-audio.play();
+const playMusic = (track) => {
+    // let audio = new Audio("/songs/" + track)
+     currentSong.src = "/songs/" + track
+    currentSong.play();
+    play.src="images/pause.svg"
+    document.querySelector(".songinfo").innerHTML = track
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
 }
-async function main(){
-    let currentSong;
+async function main() {
+    
+
     //gett the list of all the songs
     let songs = await getSongs()
 
     //show all the songs in the playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
-      songUL.innerHTML = songUL.innerHTML + `<li> 
+        songUL.innerHTML = songUL.innerHTML + `<li> 
       
                             <img src="images/music.svg" alt="">
                             <div class="info">
@@ -35,15 +42,27 @@ async function main(){
                             <div class="playnow">
                                 <span>Play Now</span>
                                 <img class="invert" src="images/play.svg" alt="">
-                            </div></li>`;  
+                            </div></li>`;
     }
-//attch an event listner to an each song
-Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
-    e.addEventListener("click", element=>{
-        console.log(e.querySelector(".info").firstElementChild.innerHTML)
-        playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    //attch an event listner to an each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
     })
-})
+
+    //atach an event listener to play next and previous
+    play.addEventListener("click", ()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src="images/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src="images/play.svg"
+        }
+    })
 
 
 }
