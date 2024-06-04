8eb2657603bf40eed065ce2d721460c0ca90ea4d
Himani Babariya <himanibabariya@gmail.com>
2024-06-04 00:24:41 -0400

    Made it responsive

--- script.js
+++ script.js
@@ -100,7 +100,12 @@ async function main() {
 
     //Add an event listner for hamburger
     document.querySelector(".hamburger").addEventListener("click", ()=>{
-        document.querySelector(".left").style.left = 0
+        document.querySelector(".left").style.left = "0"
+    })
+
+    //Add an event listner to close button
+    document.querySelector(".close").addEventListener("click", ()=>{
+        document.querySelector(".left").style.left = "-110%";
     })
 
 }
