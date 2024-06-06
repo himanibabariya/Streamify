fde72d22cc839a61f200812a7fbbcdb12357e54b
Himani Babariya <himanibabariya@gmail.com>
2024-06-05 23:43:26 -0400

    Previous and Next song buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let currentSong = new Audio();
+let songs;
 
 function secondsToMinutesSeconds(seconds) {
     if (isNaN(seconds) || seconds < 0) {
@@ -30,20 +31,20 @@ async function getSongs() {
     }
     return songs
 }
-const playMusic = (track, pause=false) => {
+const playMusic = (track, pause = false) => {
     // let audio = new Audio("/songs/" + track)
-     currentSong.src = "/songs/" + track
+    currentSong.src = "/songs/" + track
 
-     if(!pause){
-         currentSong.play();
-         play.src="images/pause.svg"
-     }
+    if (!pause) {
+        currentSong.play();
+        play.src = "images/pause.svg"
+    }
     document.querySelector(".songinfo").innerHTML = decodeURI(track)
     document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
 }
 async function main() {
     //gett the list of all the songs
-    let songs = await getSongs()
+    songs = await getSongs()
     playMusic(songs[0], true)
 
     //show all the songs in the playlist
@@ -70,25 +71,25 @@ async function main() {
     })
 
     //atach an event listener to play next and previous
-    play.addEventListener("click", ()=>{
-        if(currentSong.paused){
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
             currentSong.play()
-            play.src="images/pause.svg"
+            play.src = "images/pause.svg"
         }
-        else{
+        else {
             currentSong.pause()
-            play.src="images/play.svg"
+            play.src = "images/play.svg"
         }
     })
 
 
     //listen for time update event
-    currentSong.addEventListener("timeupdate", ()=>{
+    currentSong.addEventListener("timeupdate", () => {
         console.log(currentSong.currentTime, currentSong.duration)
         document.querySelector(".songtime").innerHTML = `$
         {secondsToMinutesSeconds(currentSong.currentTime)}/$
         {secondsToMinutesSeconds(currentSong.duratio)}`
-        document.querySelector(".circle").style.left = (currentSong.currentTime/ currentSong.duration) * 100 + "%";
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
     })
 
     //Add an event listenr to seekbar
@@ -99,13 +100,30 @@ async function main() {
     })
 
     //Add an event listner for hamburger
-    document.querySelector(".hamburger").addEventListener("click", ()=>{
+    document.querySelector(".hamburger").addEventListener("click", () => {
         document.querySelector(".left").style.left = "0"
     })
 
     //Add an event listner to close button
-    document.querySelector(".close").addEventListener("click", ()=>{
+    document.querySelector(".close").addEventListener("click", () => {
         document.querySelector(".left").style.left = "-110%";
+    })
+
+    //add an event listner to previous 
+    previous.addEventListener("click", () => {
+        currentSong.pause()
+        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+        if ((index - 1 >= 0)) {
+            playMusic(songs[index + 1])
+        }
+    })
+    //add an event listner to next
+    next.addEventListener("click", () => {
+        currentSong.pause()
+        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+        if ((index + 1 < songs.length )) {
+            playMusic(songs[index + 1])
+        }
     })
 
 }
